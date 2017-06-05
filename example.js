4d4cb7ae7f31f243a90d79b869e8d698aebd252d
Sivan Mehta <Sivanmehta2@gmail.com>
2017-06-04 22:46:00 -0400

    add put and get functions

--- example.js
+++ example.js
@@ -2,6 +2,13 @@ const gs = require('./')
 const config = require('./config.json')
 
 var db = new gs({ token: config.token })
-db.listGistDescriptions(console.log)
 
-db.create("db-somedata")
+const data = {
+  dbname: 'db-something',
+  key: 'Go',
+  value: 'Knicks'
+}
+
+db.create(data.dbname)
+db.put(data)
+db.get(data, console.log)
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const GitHubApi = require('github')
 var levelup = require('levelup')
+var leveldown = require('leveldown')
 
 function GHStorage (config) {
   var defaultConfig = {
@@ -35,9 +36,26 @@ GHStorage.prototype.listGistDescriptions = function (cb) {
 /**
  * Create a new datastore with the given name
  * @param {String} dbname name of datastore
-*/
-GHStorage.prototype.create = function (dbname) {
-  this.dbs[dbname] = levelup(dbname)
+ * @param {Function} cb function of the form function (err, db) {}
+ */
+GHStorage.prototype.create = function (dbname, cb) {
+  this.dbs[dbname] = levelup(dbname, cb)
+}
+
+/**
+ * Create a new datastore with the given name
+ * @param {Object} data
+ * @param {String} data.key
+ * @param {String} data.value
+ * @param {Function} callback
+ * @param {Object}
+ */
+GHStorage.prototype.put = function (data, callback) {
+  this.dbs[data.dbname].put(data.key, data.value, callback)
+}
+
+GHStorage.prototype.get = function (data, callback) {
+  this.dbs[data.dbname].get(data.key, callback)
 }
 
 module.exports = GHStorage
